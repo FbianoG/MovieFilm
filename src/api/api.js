6e468e63bc9ff4601ff3d750f6938db01cc5eef8
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-27 14:50:12 -0300

    update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 // 04e7016b02b7bbb881282e73c4c81bf1
 
 
-const UrlBack = "http://localhost:3000"
-// const UrlBack = "https://movie-film-back.vercel.app"
+// const UrlBack = "http://localhost:3000"
+const UrlBack = "https://movie-film-back.vercel.app"
 
 
 
